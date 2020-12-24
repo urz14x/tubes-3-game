753b0c80543f648a2e7c08a9045a76538da8913c
ustamirazib45 <ustamirazib45@gmail.com>
2020-12-25 00:27:42 +0700

    first commit

---
+++ button.py
@@ -0,0 +1,31 @@
+import pygame.font
+
+
+class Button:
+
+    def __init__(self, ai_game, msg):
+        """Initialize button attributes."""
+        self.screen = ai_game.screen
+        self.screen_rect = self.screen.get_rect()
+
+        # Set dimensi
+        self.width, self.height = 200, 70
+        self.button_color = "#6FD08C"
+        self.text_color = "#ffffff"
+        self.font = pygame.font.SysFont(None, 48)
+
+        self.rect = pygame.Rect(100, 0, self.width, self.height)
+        self.rect.center = self.screen_rect.center
+
+        self._prep_msg(msg)
+
+    def _prep_msg(self, msg):
+        self.msg_image = self.font.render(msg, True, self.text_color,
+                                          self.button_color)
+        self.msg_image_rect = self.msg_image.get_rect()
+        self.msg_image_rect.center = self.rect.center
+
+    def draw_button(self):
+        # Draw blank button and then draw message.
+        self.screen.fill(self.button_color, self.rect)
+        self.screen.blit(self.msg_image, self.msg_image_rect)
